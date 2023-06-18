c06a32339f995adff9efe56f81fc5fcb88446e9d
pipecove <pipecove@gmail.com>
2023-06-18 02:18:15 -0500

    Clases 00, 01 y 03

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,20 @@
+var lastName = 'Felipe';
+lastName = 'Andres';
+console.log(lastName);
+
+let fruit = 'apple';
+fruit = 'kiwi';
+console.log(fruit);
+
+const animal = 'dog';
+animal = 'cat';
+
+
+const fruis = () => {
+    if (true){
+        var fruit1 = 'apple'; // functio scope
+        let fruit2 = 'kiwi'; // block scope
+        const fruit3 = 'banana'; //block scope
+    }
+}
+
---
+++ src/es6/01-arrow-function.js
@@ -0,0 +1,9 @@
+function square(num){
+    return num * num;
+}
+
+const squere = (num) => { // funcion flecha
+    return num * num;
+}
+
+const square =  num * num; // funcion sin necesidad de tener el return ni los parentecis

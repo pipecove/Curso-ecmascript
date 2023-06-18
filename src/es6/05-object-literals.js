f6480f83becfde74501f2f47010f87574e431741
pipecove <pipecove@gmail.com>
2023-06-18 04:13:46 -0500

    Se agregaron las clases 05 y 06

---
+++ src/es6/05-object-literals.js
@@ -0,0 +1,13 @@
+// enahced object leterals
+
+function newUser(user, age, country, uId){
+    return{
+        user,
+        age,
+        country,
+        uId
+    }
+}
+
+console.log(newUser('pipecove', 26, 'CO', 1));
+
---
+++ src/es6/06-promises.js
@@ -0,0 +1,13 @@
+const anotherFunction = () => {
+    return new Promise((resolve, reject) => {
+        if(true){
+            resolve('hey!!');
+        } else {
+            reject('paila');
+        }
+    })
+}
+
+anotherFunction()
+    .then(response => console.log(response))
+    .catch(err => console.log(err));

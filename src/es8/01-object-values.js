04a0aad66f034bd226a340a9798d6b753feb89e1
pipecove <pipecove@gmail.com>
2023-06-19 11:15:01 -0500

    Agregamos las clases de ES8 de object.entries y .values

---
+++ src/es8/01-object-values.js
@@ -0,0 +1,6 @@
+const countries = { MX: 'Mexico', CO: 'Colombia', CL: 'Chile', PE: 'Peru'};
+
+console.log(Object.values(countries));
+
+//en este caso object tambien tiene la primera en mayuscula 
+//Solo trae el valor y ignora las llaves, nos da un array de strings
---
+++ src/es8/object-entries.js
@@ -0,0 +1,6 @@
+const countries = { MX: 'Mexico', CO: 'Colombia', CL: 'Chile', PE: 'Peru'};
+
+console.log(Object.entries(countries));
+
+//trasnforma el objeto en un array don de cada uno de los elementos es la representacion
+//object la primera letra es en mayuscula

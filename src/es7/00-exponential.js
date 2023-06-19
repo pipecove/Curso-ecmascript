d13d5db31da3baef621becf7652bd777bfa8d2bf
pipecove <pipecove@gmail.com>
2023-06-19 11:04:53 -0500

    Agregamos las clases de exponencial e includes

---
+++ src/es7/00-exponential.js
@@ -0,0 +1,5 @@
+// 3 a potencia 4
+const data = 3 ** 4;
+
+console.log(data);
+
---
+++ src/es7/01-arrays-include.js
@@ -0,0 +1,7 @@
+let numbers = [1, 2, 3, 4, 5];
+
+console.log(numbers.includes(4));
+
+let list = ['oscar', 'david', 'ana'];
+
+console.log(list.includes('oscar'));

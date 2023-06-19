3f90f5c6d491eebaa139c91d948859fc3e90f706
pipecove <pipecove@gmail.com>
2023-06-19 17:20:53 -0500

    Agregamos las clases de ES13

---
+++ src/es13/00-at.js
@@ -0,0 +1,4 @@
+const array = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+console.log(array[array.length -1]);//formula para ver la ultima posicion del array
+console.log(array.flat(-1));//para ver la ultima posicion del array 
